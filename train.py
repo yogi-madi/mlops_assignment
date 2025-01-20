3f0a10cb266aaf227c8665cdd900060ff035b9d3
Sasi <2023aa05271@wilp.bits-pilani.ac.in>
2025-01-20 12:27:03 +0530

    Added sample project and CI/CD workflow

---
+++ train.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Sample data
+X = np.array([[1], [2], [3]])
+y = np.array([1, 2, 3])
+
+model = LinearRegression()
+model.fit(X, y)
+
+print("Model trained successfully!")
